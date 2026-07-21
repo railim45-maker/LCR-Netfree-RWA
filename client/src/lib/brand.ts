b2faf62883b90a6b761759448090eb5235f3caa8
Dario Colognese <railim45@gmail.com>
2026-07-21 22:22:08 +0200

    Add APP_TITLE constant for application title

--- client/src/lib/brand.ts
+++ client/src/lib/brand.ts
@@ -1,3 +1,4 @@
 export const BRAND_NAME = "Railim Nativa Donativa";
 export const BRAND_TAGLINE = "Economia del Dono e Tokenizzazione RWA";
 export const RAILIM_LOGO_URL = "";
+export const APP_TITLE = "Railim Nativa Donativa";
