6fc2b9f41f878b81890751c9cf4fbdf4be871f87
Dario Colognese <railim45@gmail.com>
2026-07-21 22:24:30 +0200

    Add APP_VERSION constant to brand.ts

--- client/src/lib/brand.ts
+++ client/src/lib/brand.ts
@@ -2,3 +2,4 @@ export const BRAND_NAME = "Railim Nativa Donativa";
 export const BRAND_TAGLINE = "Economia del Dono e Tokenizzazione RWA";
 export const RAILIM_LOGO_URL = "";
 export const APP_TITLE = "Railim Nativa Donativa";
+export const APP_VERSION = "1.0.0";
