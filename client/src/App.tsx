785da9d538067e7ec80c2a934ac31ece5884b9c3
Dario Colognese <railim45@gmail.com>
2026-07-25 20:01:00 +0200

    Remove NetFree routes from App component
    Removed routes for NetFree pages from the App component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,5 @@
 import { Switch, Route } from "wouter";
 import Home from "@/pages/Home";
-import AcquaPage from "@/pages/Acqua.tsx";
-import TerraPage from "@/pages/Terra.tsx";
-import EnergiaPage from "@/pages/Energia.tsx";
-import FormazionePage from "@/pages/Formazione.tsx";
 import ClubDeal from "@/pages/ClubDeal";
 import NotFound from "@/pages/not-found";
 
@@ -12,14 +8,6 @@ export default function App() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/club-deal" component={ClubDeal} />
-      
-      {/* Rotte NetFree Dedicate */}
-      <Route path="/netfree/acqua" component={AcquaPage} />
-      <Route path="/netfree/terra" component={TerraPage} />
-      <Route path="/netfree/energia" component={EnergiaPage} />
-      <Route path="/netfree/formazione" component={FormazionePage} />
-
-      {/* Fallback 404 */}
       <Route component={NotFound} />
     </Switch>
   );
