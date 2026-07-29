be944c05135c5d3456b3b02ccf831ff528164743
Dario Colognese <railim45@gmail.com>
2026-07-29 12:29:48 +0200

    Add route for ProspettoRiservatoPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "wouter";
 import Home from "@/pages/Home";
 import TokenizzazionePage from "@/pages/TokenizzazionePage";
 import EconomiaDonoPage from "@/pages/EconomiaDonoPage";
+import ProspettoRiservatoPage from "@/pages/ProspettoRiservatoPage";
 import NotFound from "@/pages/NotFound";
 
 function Router() {
@@ -10,6 +11,7 @@ function Router() {
       <Route path="/" component={Home} />
       <Route path="/tokenizzazione" component={TokenizzazionePage} />
       <Route path="/economia-dono" component={EconomiaDonoPage} />
+      <Route path="/prospetto-riservato" component={ProspettoRiservatoPage} />
       <Route component={NotFound} />
     </Switch>
   );
