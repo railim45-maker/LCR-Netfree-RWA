23ea7d412e13d279c28853b2de6395a3970add71
Dario Colognese <railim45@gmail.com>
2026-07-31 20:41:47 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import { Route, Switch } from 'wouter'; // o il router che stai usando (es. react-router-dom)
-import Home from './pages/Home';
-import TokenizzazionePage from './pages/TokenizzazionePage';
-import AdminDashboard from './pages/AdminDashboard';
-import FloatingOracolo from './pages/FloatingOracolo';
+import { Switch, Route } from "wouter";
+import Home from "./pages/Home";
+import NetFreeDeepDive from "./pages/NetFreeDeepDive";
+import NotFound from "./pages/not-found";
 
 export default function App() {
   return (
-    <div className="relative min-h-screen bg-[#fcfbf9]">
-      {/* ROUTER DELLE PAGINE */}
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/tokenizzazione" component={TokenizzazionePage} />
-        <Route path="/admin" component={AdminDashboard} />
-        {/* Fallback per pagine non trovate */}
-        <Route>
-          <div className="p-12 text-center font-serif">Pagina non trovata. Torna alla <a href="/" className="underline text-amber-800">Home</a></div>
-        </Route>
-      </Switch>
-
-      {/* ORÀCOLO FLUTTUANTE PRESENTE SU TUTTO IL SITO */}
-      <FloatingOracolo />
-    </div>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/netfree" component={NetFreeDeepDive} />
+      {/* Fallback per rotte non trovate */}
+      <Route component={NotFound} />
+    </Switch>
   );
 }
