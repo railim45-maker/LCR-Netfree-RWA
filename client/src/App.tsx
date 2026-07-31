ab597f4cfc25dea09235eed17d21f991047d66ae
Dario Colognese <railim45@gmail.com>
2026-07-31 20:58:59 +0200

    Refactor routing in App component
    Removed TokenizzazionePage and NotFound routes, added fallback to Home.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,14 @@
 import { Switch, Route } from "wouter";
 import Home from "./pages/Home";
 import NetFreeDeepDive from "./pages/NetFreeDeepDive";
-import TokenizzazionePage from "./pages/TokenizzazionePage"; // Assicurati di avere questo file o crealo
-import NotFound from "./pages/not-found";
 
 export default function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/netfree" component={NetFreeDeepDive} />
-      <Route path="/tokenizzazione" component={TokenizzazionePage} />
-      {/* Fallback per rotte non trovate */}
-      <Route component={NotFound} />
+      {/* Fallback di sicurezza direttamente sulla Home */}
+      <Route component={Home} />
     </Switch>
   );
 }
