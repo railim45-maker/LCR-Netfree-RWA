c3115755ec0fd1e99509e2cdfd48826b58eae88b
Dario Colognese <railim45@gmail.com>
2026-08-01 15:00:56 +0200

    Fix import and route registration for NetFreePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import { Switch, Route } from "wouter";
 import Home from "@/pages/Home";
-import TokenizzazionePage from "@/pages/TokenizzazionePage"; // <-- 1. IMPORTA IL FILE
-import NetFreePage from "@/pages/nefreedive";
+import TokenizzazionePage from "@/pages/TokenizzazionePage";
+import NetFreePage from "@/pages/NetFreePage"; // <-- Assicurati che il nome del file corrispondi esattamente (es. NetFreePage o nefreedive)
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/tokenizzazione" component={TokenizzazionePage} /> {/* <-- 2. REGISTRA LA ROTTA */}
+      <Route path="/tokenizzazione" component={TokenizzazionePage} />
       <Route path="/netfree" component={NetFreePage} />
     </Switch>
   );
 }
+
+export default Router;
