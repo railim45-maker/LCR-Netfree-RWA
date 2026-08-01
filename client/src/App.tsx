6a48d370736eb19d05f9baf5b396bd32bc21b3eb
Dario Colognese <railim45@gmail.com>
2026-08-01 18:29:29 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,9 @@ import TokenizzazionePage from "@/pages/TokenizzazionePage";
 import NetFreePage from "@/pages/NetFreePage";
 import LibertaLegalePage from "@/pages/LibertaLegalePage";
 import PergamenaPage from "@/pages/PergamenaPage";
-import AdminDashboard from "@/pages/AdminDashboard"; // <-- Il componente Admin completo che hai inserito
+import BibliotecaPage from "@/pages/BibliotecaPage";
+import TestConsapevolezzaPage from "@/pages/TestConsapevolezzaPage";
+import AdminDashboard from "@/pages/AdminDashboard";
 import OracoloWidget from "@/components/OracoloWidget";
 
 function Router() {
@@ -16,9 +18,10 @@ function Router() {
         <Route path="/netfree" component={NetFreePage} />
         <Route path="/liberta-legale" component={LibertaLegalePage} />
         <Route path="/pergamena" component={PergamenaPage} />
+        <Route path="/biblioteca" component={BibliotecaPage} />
+        <Route path="/test-consapevolezza" component={TestConsapevolezzaPage} />
         <Route path="/admin" component={AdminDashboard} />
       </Switch>
-      {/* L'Oracolo fluttua permanentemente su ogni pagina */}
       <OracoloWidget />
     </div>
   );
