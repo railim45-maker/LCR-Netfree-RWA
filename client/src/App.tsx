54289f715b30fcb8e530a1ea49d3a691293abdb8
Dario Colognese <railim45@gmail.com>
2026-08-01 16:48:43 +0200

    Refactor Router to include OracoloWidget
    Wrap Router component in a div and add OracoloWidget.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,20 @@ import Home from "@/pages/Home";
 import TokenizzazionePage from "@/pages/TokenizzazionePage";
 import NetFreePage from "@/pages/NetFreePage";
 import LibertaLegalePage from "@/pages/LibertaLegalePage";
+import OracoloWidget from "@/components/OracoloWidget";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/tokenizzazione" component={TokenizzazionePage} />
-      <Route path="/netfree" component={NetFreePage} />
-      <Route path="/liberta-legale" component={LibertaLegalePage} />
-    </Switch>
+    <div className="relative min-h-screen">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/tokenizzazione" component={TokenizzazionePage} />
+        <Route path="/netfree" component={NetFreePage} />
+        <Route path="/liberta-legale" component={LibertaLegalePage} />
+      </Switch>
+      {/* L'Oracolo fluttua permanentemente su ogni pagina del sito */}
+      <OracoloWidget />
+    </div>
   );
 }
 
